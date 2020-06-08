6c1749df8f6356cfd7623320318aef9135590dc2
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-06-08 11:24:18 +0100

    learning about REST, DESTRUCTURING ARRAYS, DESTRUCTURING OBJECTS, SPREAD SYNTAX

--- demo/src/scripts/index.js
+++ demo/src/scripts/index.js
@@ -1,5 +1,33 @@
+// using Rest Parameters
 function sendCars(day, ...carIds) {
     carIds.forEach(id => console.log(id));
 }
 
 sendCars('Monday', 1, 2, 3);
+
+// Destructuring Arrays
+let carId1s = [100, 200, 300, 400, 500];
+let car1, car2, remainigCars;
+
+[car1, car2, ...remainigCars] = carId1s;
+
+console.log(car1, car2, remainigCars);
+
+// Destructuring objects
+let car = {
+    id: 5000,
+    style: 'convertible'
+};
+
+({ id, style } = car);
+
+console.log(id, style);
+
+// Spread Syntax
+function startCars(car1, car2, car3, ...remainingCars) {
+    console.log(rest);
+}
+
+let cars = [1, 2, 3, 4, 5, 6];
+
+startCars(...cars);
