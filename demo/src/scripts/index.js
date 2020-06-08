e2106aa539397893d28aa0f7cc502cb9b6079294
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-06-08 14:17:54 +0100

    simple class with a method

--- demo/src/scripts/index.js
+++ demo/src/scripts/index.js
@@ -1,32 +1,15 @@
-// ARRAYS
-let carIds = [
-    { carId: 123, style: 'sedan' },
-    { carId: 456, style: 'convertible' },
-    { carId: 789, style: 'sedan' }
-];
+// CLASSES IN JAVASCRIPT
+class Car {
+    constructor(id) {
+        this.id = id;
+    }
+    identify() { // METHOD
+        return `Car Id: ${this.id}`;
+    }
+}
 
-// ForEach method
-carIds.forEach(
-    car => console.log(car));
+let car = new Car(123);
+console.log(car.id);
 
-carIds.forEach(
-    (car, index) => console.log(car, index));
-
-// Array filtering
-let convertibles = carIds.filter(
-    car => car.style === 'convertible'
-);
-console.log(convertibles);
-
-// checks if all element in the array 
-// fulfills the conditon (true or false)
-let result = carIds.every(
-    car => car.carId > 0
-);
-console.log(result);
-
-// returns the first result that matches the condition
-let car = carIds.find(
-    car => car.carId > 500
-);
-console.log(car);
+car.id = 456;
+console.log(car.identify());
