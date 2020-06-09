7499c4efa989b41fcd64ddd4c2117a166c5f2383
Simeon Olorunninwo <Simeon.olorunninwo@wemabank.com>
2020-06-09 10:10:20 +0100

    http POSt using JQuery

--- demo/src/scripts/index.js
+++ demo/src/scripts/index.js
@@ -1,9 +1,16 @@
 // USING JQUERY 'GET'
 import $ from 'jquery';
 
-let promise = $.get("http://5b32a4fd82407e001413f1df.mockapi.io/api/v1/users");
+let user = {
+    name: 'Mark Facebook',
+    avatar: 'mark.jpg'
+};
+
+let promise = $.post(
+    "http://5b32a4fd82407e001413f1df.mockapi.io/api/v1/users", user
+);
 
 promise.then(
-    data => console.log('success: ', data),
+    data => console.log('data: ', data),
     error => console.log('error: ', error)
 );
